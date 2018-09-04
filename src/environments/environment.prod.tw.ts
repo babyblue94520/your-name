462bf36aa4bb8cd7228bb0d16248c137037f1db5
babyblue94520 <babyblue94520@yahoo.com.tw>
2018-09-04 22:41:05 +0800

    update

--- src/environments/environment.prod.tw.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  lang: 'zh_TW',
-  passwordLength: 8
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,4 @@
 
 export const environment = {
   production: false,
-  lang: 'zh_TW',
-  passwordLength: 8
 };
